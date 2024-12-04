78317f3b2fb812d28cca83b3e382108baf84a306
Just1Developer <weisserbenjamin98@gmail.com>
2024-12-04 14:36:29 +0100

    Added list task

---
+++ src/edu/kit/kastel/ListTask.java
@@ -0,0 +1,15 @@
+package edu.kit.kastel;
+
+import java.util.List;
+
+public class ListTask {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static <T> List<T> toList(T[] array) {
+        return null;    // Todo
+    }
+
+}
